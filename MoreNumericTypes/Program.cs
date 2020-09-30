52fbbd6fe2e251d930b4aa6372b35b56257b660b
Monique Nguyen <moniquehanguyen@gmail.com>
2020-09-29 19:30:15 -0500

    Changed converting to int methods.

--- MoreNumericTypes/Program.cs
+++ MoreNumericTypes/Program.cs
@@ -7,9 +7,9 @@ namespace MoreNumericTypes
         static void Main(string[] args)
         {
             Console.WriteLine("How many miles have you driven?");
-            int miles = Convert.ToInt32(Console.ReadLine());
+            int miles = int.Parse(Console.ReadLine());
             Console.WriteLine("How many gallons of gas have you consumed?");
-            int gallons = Convert.ToInt32(Console.ReadLine());
+            int gallons = int.Parse(Console.ReadLine());
             int milesPerGallon = miles / gallons;
             Console.WriteLine("Your miles per gallon is " + milesPerGallon);
         }
--- NumericTypes/Program.cs
+++ NumericTypes/Program.cs
@@ -7,9 +7,9 @@ namespace NumericTypes
         static void Main(string[] args)
         {
             Console.WriteLine("What is the length?");
-            int length = Convert.ToInt32(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
             Console.WriteLine("What is the width?");
-            int width = Convert.ToInt32(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
             int area = length * width;
             Console.WriteLine("The area of this rectangle is " + area);
         }
